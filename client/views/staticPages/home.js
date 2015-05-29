d221e013aae2bb17f0f66a83a464c4b4078e8f99
alexofob <alexofob@yahoo.com>
2015-05-29 22:18:27 +0000

    Materialized home page

---
+++ client/views/staticPages/home.js
@@ -0,0 +1,20 @@
+/**
+ * Created by ealeofo on 5/28/2015.
+ */
+
+Template.home.rendered = function() {
+    var i = 1;
+    var images = ['/img/bedroom-dark-bg.jpg','/img/banner-bg.jpg','/img/house-bg-dark.jpg'];
+    var image = $('.intro-header');
+    //Initial background image setup
+    image.css('background-image', 'url(/img/bedroom-dark-bg.jpg)')
+    //Change image at regular intervals
+    setInterval(function() {
+        image.css('background-image', 'url(' + images [i++] + ')');
+        if (i == images.length)
+            i = 0;
+        }, 20000);
+    };
+
+
+
